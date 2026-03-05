24ea9c72927579b9de3cf52bd5c0f954e21c4f8d
Sasha Savenko <sashasavenko3@gmail.com>
2026-03-05 21:12:34 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      My country tales
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,11 @@
+import css from './Layout.module.css'
+
+const Layout = () => {
+  return (
+    <div className={css.layout}>
+      My best Layout
+    </div>
+  )
+}
+
+export default Layout
